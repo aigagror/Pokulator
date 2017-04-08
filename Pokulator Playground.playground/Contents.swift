eb403cacd184ef495feebba4eddac39b6bab68f6
Eddie Huang <aigagror@gmail.com>
2017-04-07 23:16:16 -0500

    Moved playground

---
+++ Pokulator Playground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum Suit: String {
+    case spades = "♠"
+    case hearts = "♥️"
+    case diamonds = "♦"
+    case clubs = "♣"
+}
+
+Suit(rawValue: "♥️")
